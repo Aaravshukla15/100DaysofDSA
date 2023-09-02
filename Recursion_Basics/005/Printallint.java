3d8f65f99f84380c4762de3641f77b7628f8601f
Aarav Shukla <104962175+Aaravshukla15@users.noreply.github.com>
2023-09-03 00:44:08 +0530

    Update Printallint.java

--- Recursion_Basics/005/Printallint.java
+++ Recursion_Basics/005/Printallint.java
@@ -5,18 +5,15 @@ public class Solution
         if(x == 0) {
             return;
         }
-        // Insert element in the array
         ans[ans.length - x] = x;
 
-        // Call recursive function
+ 
         recursiveFunction(x - 1, ans);
     }
 
     public static int[] printNos(int x) {
-        // Declaring empty integer array
         int []ans = new int[x];
 
-        // Calling recursive function
         recursiveFunction(x, ans);
 
         return ans;
