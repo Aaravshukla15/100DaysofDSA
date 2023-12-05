4f76b3ca6214044f73fd823dcf0e5a33d2f3d3a4
Aarav Shukla <104962175+Aaravshukla15@users.noreply.github.com>
2023-12-05 21:05:08 +0530

    Updated testcase example

--- PracticeQues/ArraySum.java
+++ PracticeQues/ArraySum.java
@@ -1,4 +1,7 @@
 //Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).Return the running sum of nums.
+// Input: nums = [1,2,3,4]
+// Output: [1,3,6,10]
+// Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
 
 class ArraySum {
     public int[] runningSum(int[] nums) {
