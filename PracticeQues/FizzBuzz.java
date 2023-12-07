6bc0ab55be7aecf7d12f92e83d3f78c3dead78db
Aarav Shukla <104962175+Aaravshukla15@users.noreply.github.com>
2023-12-07 22:49:05 +0530

    Solved FizzBuzz.java

---
+++ PracticeQues/FizzBuzz.java
@@ -0,0 +1,36 @@
+/* Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+
+Example 1:
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+Example 2:
+
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"] */
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> answer = new ArrayList<>(n);
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5==0){
+                answer.add("FizzBuzz");
+            }
+            else if(i%3==0){
+                answer.add("Fizz");
+            }
+            else if(i%5==0){
+                answer.add("Buzz");
+            }
+            else{
+                answer.add(String.valueOf(i));
+            }
+        }
+        return answer;
+    }
+}
