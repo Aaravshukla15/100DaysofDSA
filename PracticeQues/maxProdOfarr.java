ec550d002909190de96dd025d6317d29b7b8bc0d
Aarav Shukla <104962175+Aaravshukla15@users.noreply.github.com>
2023-12-12 10:48:16 +0530

    Create maxProdOfarr.java

---
+++ PracticeQues/maxProdOfarr.java
@@ -0,0 +1,23 @@
+// ## Maximum Product of two Elements in array.
+// Given the array of integers nums, you will choose two different 
+// indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+// Input: nums = [3,4,5,2]
+// Output: 12 
+// Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, 
+// that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12. 
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int prod=0;
+        int maxProd = 0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<i;j++){
+                prod=(nums[i]-1)*(nums[j]-1);
+                if(prod>maxProd){
+                    maxProd = prod;
+                }
+            }
+        }
+        return maxProd;
+    }
+}
