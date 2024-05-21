f93840558c9eced4cd8ad180e47192e697f65fc9
Aarav Shukla <104962175+Aaravshukla15@users.noreply.github.com>
2024-05-21 19:00:27 +0530

    Create ReverseString.java

---
+++ Strings/ReverseString.java
@@ -0,0 +1,21 @@
+// Reverse the inputed string
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        String reversed = reverseString(input);
+        System.out.println("Reversed string: " + reversed);
+        scanner.close();
+    }
+
+    public static String reverseString(String str) {
+        StringBuilder reversed = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed.append(str.charAt(i));
+        }
+        return reversed.toString();
+    }
+}
