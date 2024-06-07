14e7e2b001844b0bd3f12df3a82cdf02dc245aef
Aarav Shukla <104962175+Aaravshukla15@users.noreply.github.com>
2024-06-07 23:37:59 +0530

    Create MinElem.java

---
+++ Binary Search/MinElem.java
@@ -0,0 +1,27 @@
+// Find Minimum in Rotated Sorted Array
+// Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+// [4,5,6,7,0,1,2] if it was rotated 4 times.
+// [0,1,2,4,5,6,7] if it was rotated 7 times.
+// Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+// Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+// You must write an algorithm that runs in O(log n) time.
+// Example 1:
+// Input: nums = [3,4,5,1,2]
+// Output: 1
+// Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+// Link:https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+// Solution
+class Solution {
+    public int findMin(int[] nums) {
+        int min = nums[0];
+        for(int num:nums){
+            if(min>num){
+                min=num;
+            }
+        }
+        return min;
+    }
+}
