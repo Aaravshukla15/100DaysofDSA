23f6d240d13f478b9f8c88f9b55cdea3c68866ad
Aarav Shukla <104962175+Aaravshukla15@users.noreply.github.com>
2024-06-13 23:34:17 +0530

    Create FibonacciNo.java

---
+++ Recursion_Basics/FibonacciNo.java
@@ -0,0 +1,21 @@
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, 
+// such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+// F(0) = 0, F(1) = 1
+// F(n) = F(n - 1) + F(n - 2), for n > 1.
+// Given n, calculate F(n).
+// Example 1:
+// Input: n = 2
+// Output: 1
+// Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+//   Link:https://leetcode.com/problems/fibonacci-number/description/
+// Solution
+class Solution {
+    public int fib(int n) {
+        if(n==0 || n == 1){
+            return n;
+        }
+        int temp = fib(n-1);
+        int temp2 = fib(n-2);
+        return temp + temp2;
+    }
+}
