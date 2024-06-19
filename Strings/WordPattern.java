d88d69ccd0dc97e47179bb6312239126ac402895
Aarav Shukla <104962175+Aaravshukla15@users.noreply.github.com>
2024-06-19 23:53:33 +0530

    Create WordPattern.java

---
+++ Strings/WordPattern.java
@@ -0,0 +1,46 @@
+// Given a pattern and a string s, find if s follows the same pattern.
+
+// Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+
+ 
+
+// Example 1:
+
+// Input: pattern = "abba", s = "dog cat cat dog"
+// Output: true
+// Example 2:
+
+// Input: pattern = "abba", s = "dog cat cat fish"
+// Output: false
+//   Link:https://leetcode.com/problems/word-pattern/description/
+// Solution
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+         String[] words = s.split(" ");
+        if (pattern.length() != words.length) {
+            return false;
+        }
+
+        Map<Character, String> charToWord = new HashMap<>();
+        Map<String, Character> wordToChar = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String word = words[i];
+
+            if (charToWord.containsKey(c)) {
+                if (!charToWord.get(c).equals(word)) {
+                    return false;
+                }
+            } else {
+                if (wordToChar.containsKey(word)) {
+                    return false;
+                }
+                charToWord.put(c, word);
+                wordToChar.put(word, c);
+            }
+        }
+
+        return true;
+    }
+}
